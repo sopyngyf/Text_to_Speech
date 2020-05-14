737daed325f61306dfc691956badfc2d89d90345
Sophia YF Ng <ng.yf.sophia@gmail.com>
2020-05-14 12:18:16 +0800

    Updated the sample text

--- Text-to-Speech-1.py
+++ Text-to-Speech-1.py
@@ -5,15 +5,20 @@ from gtts import gTTS
 import os
 
 # Language you want to convert the audio to
-language = 'en-UK'
+language = 'en-US'
 
-# ---------------------------- DO NOT TOUCH THE ABOVE PARTS --------------------------------- #
+# ----------------------- THE TEXT YOU WANT TO CONVERT ---------------------------- #
 
 # The text you want to convert to audio
-mytext = 'Replace this chunk with your desired text'
+mytext = 'Opera refers to a dramatic art form, originating in Europe, in which the emotional content is conveyed to the audience as much through music, both the vocal and instrumental, as it is through the lyrics. By contrast, in musical theatre an actors dramatic performance is primary, and the music plays a lesser role. The drama in opera is presented using the primary elements of theatre such as scenery, costumes, and acting. However, the words of the opera, or libretto, are sung rather than spoken. The singers are accompanied by a musical ensemble ranging from a small instrumental ensemble to a full symphonic orchestra.'
 
-# Passing the text and language to the engine, where marked slow=false. This tells the module that the converted audio should have a high speed
+# ----------------------- TELLING THE SCRIPT TO RUN ---------------------------- #
+
+# Passing the text and language to the engine, where slow=false. 
+# This tells the module that the converted audio should have a high speed
 myobj = gTTS(text=mytext, lang=language, slow=False)
+
+# ----------------------- REPLACE TEST1 WITH THE FILE NAME YOU WANT ------------------------- #
 
 #Saving the converted audio in a MP3 file named welcome
 myobj.save("Test1.mp3")
