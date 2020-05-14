59a17822d1785712356e93e8bb60dc097f48892f
Sophia YF Ng <ng.yf.sophia@gmail.com>
2020-05-14 11:54:41 +0800

    code edited

--- Text-to-Speech-1.py
+++ Text-to-Speech-1.py
@@ -1,14 +1,16 @@
-# Import the required module for text to speech conversion
+# Import the required module for text to speech conversion 
 from gtts import gTTS
 
-#Import module so that can play converted audio
+# Import module to play the converted audio
 import os
-
-# The text you want to convert to audio
-mytext = 'Replace this chunk with your desired text'
 
 # Language you want to convert the audio to
 language = 'en-UK'
+
+# ---------------------------- DO NOT TOUCH THE ABOVE PARTS --------------------------------- #
+
+# The text you want to convert to audio
+mytext = 'Replace this chunk with your desired text'
 
 # Passing the text and language to the engine, where marked slow=false. This tells the module that the converted audio should have a high speed
 myobj = gTTS(text=mytext, lang=language, slow=False)
